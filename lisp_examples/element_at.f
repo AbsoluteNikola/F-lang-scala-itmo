6eca6820383f823e8684a0eb4b7c4041ad48f50d
BelehovEgor <belehov.egor2001@yandex.ru>
2024-02-19 19:43:41 +0300

    added filter

--- lisp_examples/element_at.f
+++ lisp_examples/element_at.f
@@ -5,3 +5,11 @@
 
 ; (print (element-at (quote (1 2 3)) 1))
 ; (print (element-at '(1 2 3) 1))
+
+(func filter (ls pred)
+	(setq h (head ls))
+	(cond (isnull h)
+		return ()
+		return (cond (eval pred h)
+			(cons h (filter (tail ls) pred))
+			(filter (tail ls) pred))))
