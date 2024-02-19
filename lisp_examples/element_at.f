5f30ca81962ca776bdbaf16d54f311a170f15f00
BelehovEgor <belehov.egor2001@yandex.ru>
2024-02-19 19:46:10 +0300

    added map

--- lisp_examples/element_at.f
+++ lisp_examples/element_at.f
@@ -7,9 +7,16 @@
 ; (print (element-at '(1 2 3) 1))
 
 (func filter (ls pred)
-	(setq h (head ls))
+	((setq h (head ls))
 	(cond (isnull h)
 		return ()
 		return (cond (eval pred h)
 			(cons h (filter (tail ls) pred))
-			(filter (tail ls) pred))))
+			(filter (tail ls) pred)))))
+
+(func map (ls fun)
+	((setq h (head ls))
+	(cond (isnull h)
+		return ()
+		return (cons (eval (fun h)) (map (tail ls) fun)))))
+		
