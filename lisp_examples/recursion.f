d2301522df867923696ac34075d10e965efd00dc
BelehovEgor <belehov.egor2001@yandex.ru>
2024-04-23 19:16:35 +0300

    added simple recursion test

---
+++ lisp_examples/recursion.f
@@ -0,0 +1,10 @@
+(func get_n_fibonacci (n)
+    ((func helper (a b n)
+        (cond (greater n 0)
+            (helper b (plus a b) (minus n 1))
+            (a)))
+    (helper 1 1 n)))
+
+(print get_n_fibonacci(1))
+(print get_n_fibonacci(4))
+(print get_n_fibonacci(7))
